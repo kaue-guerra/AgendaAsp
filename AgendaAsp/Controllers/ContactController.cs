93e0085fe1120252e189cd1ea119c146f0784f49
Kauê Guerra <kaueguerra@gmail.com>
2022-06-12 23:22:54 -0300

    Create layout for pages "Create,Edit and delete"

--- AgendaAsp/Controllers/ContactController.cs
+++ AgendaAsp/Controllers/ContactController.cs
@@ -8,5 +8,17 @@ namespace AgendaAsp.Controllers
         {
             return View();
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
     }
 }
